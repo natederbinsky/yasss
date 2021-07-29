ebe120f0737968cf3a394f1e212433bd4792859f
Nate Derbinsky <nate.derbinsky@gmail.com>
2021-07-29 10:47:50 -0400

    take 2

--- yasss/gen.py
+++ yasss/gen.py
@@ -56,10 +56,10 @@ def build(templ_name: str, templ_dir: str, site_dir: str, destination: str, page
             SITEPREFIX: FileSystemLoader(site_dir)
         }),
         autoescape=select_autoescape(enabled_extensions=('htm', 'html')),
-        globals={
-            'template_fname': lambda s: s._TemplateReference__context.name[s._TemplateReference__context.name.rfind('/')+1:],
-        }
     )
+    env.globals.update(**{
+        'template_fname': lambda s: s._TemplateReference__context.name[s._TemplateReference__context.name.rfind('/')+1:],
+    })
     env.globals.update(**globals)
 
     for fname in pages:
